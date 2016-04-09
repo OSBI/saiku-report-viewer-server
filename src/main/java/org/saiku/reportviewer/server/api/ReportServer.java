11f80e5352f64c81befeea214eb690a6237024f2
Bruno Gama Catao <brunogamacatao@gmail.com>
2016-04-09 06:03:15 -0300

    Added the remaining API methods

--- src/main/java/org/saiku/reportviewer/server/api/ReportServer.java
+++ src/main/java/org/saiku/reportviewer/server/api/ReportServer.java
@@ -1,13 +1,25 @@
 package org.saiku.reportviewer.server.api;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.activation.DataHandler;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Map;
 
 @Path("/")
 public interface ReportServer {
+  @POST
+  @Path("/upload")
+  @Consumes(MediaType.MULTIPART_FORM_DATA)
+  Response uploadPRPTFile(DataHandler data) throws Exception;
+
   @GET
-  @Path("/html")
-  @Produces("application/xml")
-  String exportReportToHTML() throws Exception;
+  @Path("/list")
+  @Produces("application/json")
+  String listUploadedFiles() throws Exception;
+
+  @GET
+  @Path("/render")
+  @Produces("text/html")
+  String render(String reportId, String outputFormat, Map<String, String> params) throws Exception;
 }
--- src/main/java/org/saiku/reportviewer/server/api/ReportServerImpl.java
+++ src/main/java/org/saiku/reportviewer/server/api/ReportServerImpl.java
@@ -1,8 +1,87 @@
 package org.saiku.reportviewer.server.api;
 
+import java.io.*;
+import java.net.URL;
+import java.util.Map;
+
+import com.google.common.io.Files;
+import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
+import org.pentaho.reporting.engine.classic.core.MasterReport;
+import org.pentaho.reporting.engine.classic.core.modules.output.table.html.HtmlReportUtil;
+import org.pentaho.reporting.libraries.resourceloader.ResourceManager;
+
+import javax.activation.DataHandler;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.core.Response;
+
 public class ReportServerImpl implements ReportServer {
+  private static File reportsRoot;
+
   @Override
-  public String exportReportToHTML() throws Exception {
-    return "<html><body><h1>Hello World 4</h1></body></html>";
+  public String render(String reportId, String outputFormat, Map<String, String> params) throws Exception {
+    ClassicEngineBoot.getInstance().start();
+
+    URL url = ReportServerImpl.class.getResource("/basic_sample.prpt");
+    ResourceManager mgr = new ResourceManager();
+    MasterReport report = (MasterReport) mgr.createDirectly(url, MasterReport.class).getResource();
+
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(bos, true);
+
+    HtmlReportUtil.createStreamHTML(report, ps);
+
+    ps.flush();
+    bos.flush();
+
+    return bos.toString();
+  }
+
+  @Override
+  public Response uploadPRPTFile(DataHandler data) throws Exception {
+    File root = getReportsRoot();
+
+    InputStream input = data.getInputStream();
+    OutputStream output = new FileOutputStream(new File(root, data.getName()));
+
+
+    byte[] bytes = new byte[1024];
+    int read;
+
+    while((read = input.read(bytes)) > 0) {
+      output.write(bytes, 0, read);
+    }
+
+    input.close();
+    output.flush();
+    output.close();
+
+    String result = "File saved successfully";
+
+    return Response.status(200).entity(result).build();
+  }
+
+  @Override
+  public String listUploadedFiles() throws Exception {
+    StringBuilder result = new StringBuilder("[");
+
+    File root = getReportsRoot();
+    for (File f : root.listFiles()) {
+      if (result.length() > 1) {
+        result.append(",");
+      }
+      result.append(f.getName());
+    }
+
+    result.append("]");
+
+    return result.toString();
+  }
+
+  private File getReportsRoot() {
+    if (reportsRoot == null) {
+      reportsRoot = Files.createTempDir();
+    }
+
+    return reportsRoot;
   }
 }
