06c3cd4920cdc9f049dcfee5b51b9b95b9531de5
Tom Barber <tom@analytical-labs.com>
2016-04-12 13:38:00 +0100

    stuff

---
+++ reporting-megabundle/src/main/java/org/saiku/ReportingService.java
@@ -0,0 +1,17 @@
+package org.saiku;
+
+import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
+import org.pentaho.reporting.libraries.resourceloader.ResourceManager;
+
+public class ReportingService{
+
+  ResourceManager mgr;
+
+
+  public ReportingService() {
+    /*ClassicEngineBoot.getInstance().start();
+    mgr = new ResourceManager();
+    mgr.registerDefaults();*/
+  }
+
+}
