8fab06280f3aa231c36620f4951dd8f310a5a1d1
Bruno Gama Catao <brunogamacatao@gmail.com>
2016-04-12 03:10:43 -0300

    Did some initialization 'by hand' just to test service output

--- reporting-viewer/src/main/java/org/saiku/reportviewer/server/api/ReportServerImpl.java
+++ reporting-viewer/src/main/java/org/saiku/reportviewer/server/api/ReportServerImpl.java
@@ -11,6 +11,7 @@ import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
 import org.pentaho.reporting.engine.classic.core.MasterReport;
 import org.pentaho.reporting.engine.classic.core.modules.output.table.html.HtmlReportUtil;
 import org.pentaho.reporting.libraries.resourceloader.ResourceManager;
+import org.pentaho.reporting.libraries.resourceloader.ResourceFactory;
 import org.pentaho.reporting.libraries.resourceloader.factory.drawable.DrawableResourceFactory;
 import org.pentaho.reporting.libraries.resourceloader.factory.image.ImageResourceFactory;
 import org.pentaho.reporting.libraries.resourceloader.factory.property.PropertiesResourceFactory;
@@ -26,11 +27,30 @@ import javax.ws.rs.core.Response;
 public class ReportServerImpl implements ReportServer {
   private static File reportsRoot;
   ResourceManager mgr;
+
   public void init(){
     ClassicEngineBoot.getInstance().start();
     mgr = new ResourceManager();
-    //mgr.registerDefaults();
+
+    mgr.registerLoader(new URLResourceLoader());
+    mgr.registerLoader(new FileResourceLoader());
+    mgr.registerLoader(new RawResourceLoader());
+    mgr.registerLoader(new ClassloaderResourceLoader());
+    mgr.registerLoader(new ZipResourceLoader());
+
+    ResourceFactory factory = new PropertiesResourceFactory();
+    factory.initializeDefaults();
+    mgr.registerFactory(factory);
+
+    factory = new DrawableResourceFactory();
+    factory.initializeDefaults();
+    mgr.registerFactory(factory);
+
+    factory = new ImageResourceFactory();
+    factory.initializeDefaults();
+    mgr.registerFactory(factory);
   }
+
   @Override
   public String render(/*String reportId, String outputFormat, Map<String, String> params*/) throws Exception {
     byte[] reportBytes = IOUtils.toByteArray(ReportServerImpl.class.getResourceAsStream("/basic_sample.prpt"));
@@ -38,20 +58,6 @@ public class ReportServerImpl implements ReportServer {
 
 
    // final URL url = ReportServerImpl.class.getResource("/basic_sample.prpt");
-
-
-   /* mgr.registerLoader(new URLResourceLoader());
-    mgr.registerLoader(new FileResourceLoader());
-    mgr.registerLoader(new RawResourceLoader());
-    mgr.registerLoader(new ClassloaderResourceLoader());
-    mgr.registerLoader(new ZipResourceLoader());
-
-    //mgr.registerDefaults();
-    mgr.registerFactory(new PropertiesResourceFactory());
-    mgr.registerFactory(new DrawableResourceFactory());
-    mgr.registerFactory(new ImageResourceFactory());*/
-
-
 
 
     MasterReport report = (MasterReport) mgr.createDirectly(reportBytes, MasterReport.class).getResource();
