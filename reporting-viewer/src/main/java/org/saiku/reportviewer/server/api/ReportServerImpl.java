75b6df93a122291c240b6c29657d110d394488df
Tom Barber <tom@analytical-labs.com>
2016-04-21 13:35:29 +0100

    update fixes for Pentaho Reporting

--- reporting-viewer/src/main/java/org/saiku/reportviewer/server/api/ReportServerImpl.java
+++ reporting-viewer/src/main/java/org/saiku/reportviewer/server/api/ReportServerImpl.java
@@ -9,6 +9,7 @@ import com.google.common.io.Files;
 import org.apache.commons.io.IOUtils;
 import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
 import org.pentaho.reporting.engine.classic.core.MasterReport;
+import org.pentaho.reporting.engine.classic.core.ReportingInterface;
 import org.pentaho.reporting.engine.classic.core.modules.output.table.html.HtmlReportUtil;
 import org.pentaho.reporting.libraries.resourceloader.ResourceManager;
 import org.pentaho.reporting.libraries.resourceloader.factory.drawable.DrawableResourceFactory;
@@ -26,45 +27,28 @@ import javax.ws.rs.core.Response;
 public class ReportServerImpl implements ReportServer {
   private static File reportsRoot;
   ResourceManager mgr;
+  private ReportingInterface myservice;
+
   public void init(){
-    ClassicEngineBoot.getInstance().start();
-    mgr = new ResourceManager();
+    //ClassicEngineBoot.getInstance().start();
+    //mgr = new ResourceManager();
     //mgr.registerDefaults();
+    try {
+      render();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
+
+  public void setMyservice(org.pentaho.reporting.engine.classic.core.ReportingInterface myservice){
+  this.myservice = myservice;
+  }
+
   @Override
   public String render(/*String reportId, String outputFormat, Map<String, String> params*/) throws Exception {
-    byte[] reportBytes = IOUtils.toByteArray(ReportServerImpl.class.getResourceAsStream("/basic_sample.prpt"));
 
 
-
-   // final URL url = ReportServerImpl.class.getResource("/basic_sample.prpt");
-
-
-   /* mgr.registerLoader(new URLResourceLoader());
-    mgr.registerLoader(new FileResourceLoader());
-    mgr.registerLoader(new RawResourceLoader());
-    mgr.registerLoader(new ClassloaderResourceLoader());
-    mgr.registerLoader(new ZipResourceLoader());
-
-    //mgr.registerDefaults();
-    mgr.registerFactory(new PropertiesResourceFactory());
-    mgr.registerFactory(new DrawableResourceFactory());
-    mgr.registerFactory(new ImageResourceFactory());*/
-
-
-
-
-    MasterReport report = (MasterReport) mgr.createDirectly(reportBytes, MasterReport.class).getResource();
-
-    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-    PrintStream ps = new PrintStream(bos, true);
-
-    HtmlReportUtil.createStreamHTML(report, ps);
-
-    ps.flush();
-    bos.flush();
-
-    return bos.toString();
+    return myservice.createReport("/tmp/");
     //return "<html><body><h1>It Works</h1></body></html>";
   }
 
